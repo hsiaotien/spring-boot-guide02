695d22f473e6b052ad534e52583850735da3657c
xiaotian.huang <hsiaotien@163.com>
2018-09-10 23:46:26 +0800

    init

---
+++ src/main/java/com/dev/guide02/Guide02Application.java
@@ -0,0 +1,12 @@
+package com.dev.guide02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guide02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Guide02Application.class,args);
+    }
+}
